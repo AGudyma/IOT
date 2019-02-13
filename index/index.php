c114b6c18d0868ff371097b63b611464ab17a2be
Андрей Гудыма <crowley14@gmail.com>
2019-02-13 20:44:44 +0200

    with mysql

--- index/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$content='';
-exec("mode com4: BAUD=115200 PARITY=n DATA=8 STOP=1 to=off dtr=off rts=off");
-$fp =fopen("com4", "r+");
-//$fp = fopen('/dev/ttyUSB0','r+'); //use this for Linux
-fwrite($fp, "string to send"); //write string to serial
-echo fgetc($fp);
-
-fclose($fp);
